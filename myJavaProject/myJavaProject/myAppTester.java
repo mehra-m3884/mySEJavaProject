f62d913440859de4ae3c94d1394f21ef3fe67888
Mohit Mehra <mohitdlite@gmail.com>
2020-05-22 02:10:37 -0700

    .gitignore added

---
+++ myJavaProject/myJavaProject/myAppTester.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package myJavaProject;
+
+/**
+ * @author win
+ *
+ */
+public class myAppTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
