3daf9c9b2a7682f324026ad8c06cddd47e4ad378
Mohit Mehra <mohitdlite@gmail.com>
2020-05-22 01:33:35 -0700

    myAppTester.java committed

--- myJavaProject/src/myJavaProject/myAppTester.java
+++ myJavaProject/src/myJavaProject/myAppTester.java
@@ -7,6 +7,7 @@ package myJavaProject;
  * @author win
  *
  */
+import java.util.Scanner;
 public class myAppTester {
 
 	/**
@@ -14,7 +15,15 @@ public class myAppTester {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Hello, Software Engg world");
+		System.out.println("Enter a number");
+		int n = input.nextInt();
+		int result = n*n;
+		
+		System.out.println("The square of the number is");
+		System.out.println(result);
 	}
 
 }
